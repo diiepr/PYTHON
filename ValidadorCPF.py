7a5b8b163b3810dc6e592c627571299c08174152
Diego <digopires43@gmail.com>
2026-08-27 19:11:05 -0300

    Atualiza lógica do código

--- ValidadorCPF.py
+++ ValidadorCPF.py
@@ -5,13 +5,10 @@ while True:
     try:
         cpf = input("\nDigite um número de CPF (apenas números): ")
 
-        if len(cpf) > 11:
-            print("\nUm CPF contém apenas 11 números.")
+        if len(cpf) != 11:
+            print("\nUm CPF contém 11 números.")
             continue
 
-        if len(cpf) < 11:
-            print("Um CPF válido contém 11 digitos.")
-            continue
 
         cpf = int(cpf)
 
@@ -34,14 +31,17 @@ while True:
     numero_multiplicado = soma_dos_numeros * 10
     digito_1 = numero_multiplicado % 11
 
+    soma_dos_numeros = 0
+
     for numero in nove_cpf:
-         soma_dos_numeros += int(numero) * i
+         soma_dos_numeros += int(numero) * i_2
          i_2 -= 1
 
     soma_dos_numeros += digito_1 * 2
 
     digito_2 = (soma_dos_numeros * 10) % 11
     digito_2 = digito_2 if digito_2 <= 9 else 0
+    
     print(f"\nOs digitos são {digito_1} e {digito_2}")
 
     cpf_valido = f"{nove_cpf}{digito_1}{digito_2}"
